4f2bbad6e3819f8ee191d44f6f4fa3d6eb2426fb
Your Name <you@example.com>
2025-09-21 18:51:18 +0600

    create Spring Boot project with dependencies

---
+++ src/main/java/kg/megalab/kindergarten/KindergartenApplication.java
@@ -0,0 +1,13 @@
+package kg.megalab.kindergarten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergartenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KindergartenApplication.class, args);
+    }
+
+}
